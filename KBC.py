18b5cb4a62d870dacb350809a4114d15f9f0287c
Ankur Auti <115859079+Webdevava@users.noreply.github.com>
2023-04-26 20:50:48 +0530

    Update KBC.py

--- KBC.py
+++ KBC.py
@@ -1,5 +1,5 @@
 # Kaun Banega Crorepati (KBC)
-
+# You can add your own Q & A 
 questions = ["Which animal is known as the 'Ship of the Desert'?: ","How many days are there in a week?: ","How many letters are there in the English alphabet?: ","Rainbow consist of how many colours?: ","How many days are there in a year?: ","How many minutes are there in an hour?: "," How many seconds are there in a minute?: ","How many consonants are there in the English alphabet?: ","Name the National bird of India?: ","Name the biggest continent in the world?: ","How many continents are there in the world?: ","Name first of the primary colour?: ","Which colour symbolises peace?: ","Sun rises in the.....: "]
 
 answers = ["camel","7","26","7","365","60","60","21","peacock","asia","7","red","white","east"]
